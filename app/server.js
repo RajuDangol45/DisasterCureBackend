9e3d4a77177386b0ac09a882cb3dea569f2cf512
RajuDangol45 <dangol.raju.work@gmail.com>
2019-07-10 00:47:26 +0545

    added image hosting

--- app/server.js
+++ app/server.js
@@ -16,8 +16,9 @@ function run() {
   app.use(cors());
   app.use(errorHandler);
   app.set('baseUrl', config.backend.url);
-  // app.use(express.static('images/users'));
+  var dir = path.join(__dirname, '../');
   app.use(routes);
+  app.use(express.static(dir));
   mongoose.connect(config.orm.db.client + '://' + config.orm.db.connection.host + '/' + config.orm.db.connection.database);
   app.listen(config.server.port, config.server.host, () => {
     console.log(`api available on http://${config.server.host}:${config.server.port}`);
